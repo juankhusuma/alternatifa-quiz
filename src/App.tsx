4f59d02c028c2527f74ce8dd8cd9896d4719c77a
juankhusuma <juan.d.khusuma@gmail.com>
2022-09-23 12:57:20 +0700

    added auth

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
-import { useAuthState,useSignInWithGoogle } from "react-firebase-hooks/auth"
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth"
-import { app } from './lib/firebase'
+import { useAuthState } from "react-firebase-hooks/auth";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import { app } from "./lib/firebase";
 
-const auth = getAuth(app)
+const auth = getAuth(app);
 
 function App() {
-
   const signInWithGoogle = () => {
     const provider = new GoogleAuthProvider();
     signInWithPopup(auth, provider);
-  }
+  };
 
   const [user, loading, error] = useAuthState(auth);
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error</div>;
   return (
     <div>
-      {user ? JSON.stringify(user) : <button className='bg-red-400' onClick={signInWithGoogle}>Sign In</button>}
+      {JSON.stringify(user)}
+      {user ? (
+        <button onClick={() => signOut(auth)}>Sign Out</button>
+      ) : (
+        <button onClick={signInWithGoogle}>Sign In</button>
+      )}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
